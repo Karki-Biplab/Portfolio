399f6ec6c723ee5d534adafe134c75818dd68104
Karki-Biplab <k.nomem13@gmail.com>
2025-05-12 16:21:48 +0545

    Minor changes

--- .next/static/chunks/src_app_page_7966b1cc.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_477799d7._.js",
-  "static/chunks/node_modules_867ca9ec._.js"
-],
-    source: "dynamic"
-});
